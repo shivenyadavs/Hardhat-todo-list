205454f7884290e89171058a5267deac9e7f3af7
SHIVEN YADAV S <155547804+shivenyadavs@users.noreply.github.com>
2025-03-29 17:15:55 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
   solidity: "0.8.0",  // Ensure this is compatible with your contracts
   networks: {
     sepolia: {
-      url: "https://sepolia.infura.io/v3/1d17963741f84acaac21dbf4153a198e",
-      accounts: ["0x2942bb6ec2a8f2ba66bdbcfc5ef05e1b3306adb1c452c0cff8a4a999bec8a324"], // Replace with your actual private key
+      url: "https://sepolia.infura.io/v3/your_api_key",
+      accounts: ["your_private_key"], // Replace with your actual private key
     },
     localhost: {
       url: "http://127.0.0.1:8545",  // Hardhat node default RPC
